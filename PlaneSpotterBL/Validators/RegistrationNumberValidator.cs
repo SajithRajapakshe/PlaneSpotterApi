f45afada9ad78eeec6a1b13fad8c8f7fd491e494
Sajith Rajapakshe <s.rajapakshe@sana-commerce.com>
2023-07-29 21:55:44 +0530

    Validation logic changed for registration number

--- PlaneSpotterBL/Validators/RegistrationNumberValidator.cs
+++ PlaneSpotterBL/Validators/RegistrationNumberValidator.cs
@@ -15,7 +15,21 @@ namespace PlaneSpotterBL.Validators
         public override bool IsValid(object? value)
         {
             var regNumber = Convert.ToString(value);
-            return regNumber.Length > 0 ? true : false;
+            return regNumber.Length > 0 &&
+                regNumber.Contains('-') &&
+                regNumber.Split('-').Length == 2 &&
+                IsValidPrefix(regNumber.Split('-')[0]) &&
+                IsValidSuffix(regNumber.Split('-')[1]) ? true : false;
+        }
+
+        private bool IsValidPrefix(string prefix)
+        {
+            return prefix.Length == 1 || prefix.Length == 2;
+        }
+
+        private bool IsValidSuffix(string suffix)
+        {
+            return suffix.Length >= 1 && suffix.Length <= 5;
         }
 
     }
